ff96ee75b9632e40de5d7bb5df206cb3816fc11d
AlbaHoyo <alba.maria.hoyo@gmail.com>
2024-06-11 14:35:45 +0200

    v3

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
-const { dbConnection } = require('./config/config');
-const routes = require('./routes');
-app.use(express.json());
+const PORT = process.env.PORT || 8080
 
-app.use('/', routes);
+const compression = require('compression')
+const helmet = require('helmet')
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
+
+const { dbConnection } = require('./config/config')
+const routes = require('./routes')
+
+app.use(compression())
+app.use(helmet())
+
+app.use(express.json())
+
+app.use('/', routes)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs))
+
+app.get('/ping', (req, res) => {
+	res.json({ message: 'pong' })
+})
 
 
-dbConnection();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+dbConnection()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
